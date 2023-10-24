4f8d299074b507322c2b28c518de57de9772c26d
murat <mrtars2000@gmail.com>
2023-10-24 15:13:18 +0300

    variable assigment

---
+++ app/src/main/java/com/example/androidbootcampassigment1/assigments/Variable.java
@@ -0,0 +1,129 @@
+package com.example.androidbootcampassigment1.assigments;
+
+public class Variable {
+    public static void main(String[] args) {
+        String ilce = "İstanbul";
+        String kita = "Asya";
+        String faks = "123-456-7890";
+        String postaKodu = "34000";
+        String instaAdresi = "@ornek_insta";
+
+        String calistiginBolum = "Bilgi Teknolojileri";
+        int urunMiktari = 100;
+        String musteriSoyadi = "Arslan";
+        double odemeMiktari = 500.50;
+        String dogumTarihi = "01/01/1990";
+        double borc = 1000.0;
+        String medeniHal = "Bekar";
+
+        String videoYorumu = "Bu video harikaydı!";
+
+
+
+        String odemeSaati = "14:30";
+        double eftMiktari = 300.75;
+        int satilanMiktar = 50;
+        String telefonModeli = "Samsung Galaxy S21";
+
+        String dergiAdi = "Bilim ve Teknoloji Dergisi";
+        String basimTarihi = "15/09/2023";
+        double zamMiktari = 10.0;
+        int daireSayisi = 50;
+        double enlem = 41.0082;
+        double boylam = 28.9784;
+
+        String yemekAdi = "Pizza";
+        double urunFiyati = 20.99;
+
+        String sirket = "ABC Ltd.";
+        String videoAdi = "Java Programlama Dersi";
+        double muzikSuresi = 4.5;
+        int mekanPuani = 4;
+        String dosyaAdi = "video.mp4";
+        String resimFormati = "JPEG";
+
+        String renk = "Mavi";
+        String renkKodu = "#0000FF";
+
+        String bilgisayarModeli = "Dell Inspiron 15";
+        double ekranBoyutu = 15.6;
+        int kullanimSuresi = 24;
+        int basinc = 120;
+
+        String etkinlikGunu = "Pazartesi";
+        String odemeGunu = "Perşembe";
+        String yolculukCikisTarihi = "10/11/2023";
+        String mahalleAdi = "Merkez Mahallesi";
+
+        String otobusHatti = "B36";
+        int kullanilanDakika = 60;
+        String kargoTakipNo = "123456789";
+        String kuponSuresi = "31/12/2023";
+        String kuponKodu = "KUPON123";
+
+        String faturaTarihi = "05/10/2023";
+
+        System.out.println("ilce: " + ilce);
+        System.out.println("kita: " + kita);
+        System.out.println("faks: " + faks);
+
+        System.out.println("postaKodu: " + postaKodu);
+        System.out.println("instaAdresi: " + instaAdresi);
+
+        System.out.println("calistiginBolum: " + calistiginBolum);
+        System.out.println("urunMiktari: " + urunMiktari);
+        System.out.println("musteriSoyadi: " + musteriSoyadi);
+        System.out.println("odemeMiktari: " + odemeMiktari);
+        System.out.println("dogumTarihi: " + dogumTarihi);
+        System.out.println("borc: " + borc);
+        System.out.println("medeniHal: " + medeniHal);
+
+        System.out.println("videoYorumu: " + videoYorumu);
+
+
+
+        System.out.println("odemeSaati: " + odemeSaati);
+        System.out.println("eftMiktari: " + eftMiktari);
+        System.out.println("satilanMiktar: " + satilanMiktar);
+        System.out.println("telefonModeli: " + telefonModeli);
+
+        System.out.println("dergiAdi: " + dergiAdi);
+        System.out.println("basimTarihi: " + basimTarihi);
+        System.out.println("zamMiktari: " + zamMiktari);
+        System.out.println("daireSayisi: " + daireSayisi);
+        System.out.println("enlem: " + enlem);
+        System.out.println("boylam: " + boylam);
+
+        System.out.println("yemekAdi: " + yemekAdi);
+        System.out.println("urunFiyati: " + urunFiyati);
+
+        System.out.println("sirket: " + sirket);
+        System.out.println("videoAdi: " + videoAdi);
+        System.out.println("muzikSuresi: " + muzikSuresi);
+        System.out.println("mekanPuani: " + mekanPuani);
+        System.out.println("dosyaAdi: " + dosyaAdi);
+        System.out.println("resimFormati: " + resimFormati);
+
+        System.out.println("renk: " + renk);
+        System.out.println("renkKodu: " + renkKodu);
+
+        System.out.println("bilgisayarModeli: " + bilgisayarModeli);
+        System.out.println("ekranBoyutu: " + ekranBoyutu);
+        System.out.println("kullanimSuresi: " + kullanimSuresi);
+        System.out.println("basinca: " + basinc);
+
+        System.out.println("etkinlikGunu: " + etkinlikGunu);
+        System.out.println("odemeGunu: " + odemeGunu);
+        System.out.println("yolculukCikisTarihi: " + yolculukCikisTarihi);
+        System.out.println("mahalleAdi: " + mahalleAdi);
+
+        System.out.println("otobusHatti: " + otobusHatti);
+        System.out.println("kullanilanDakika: " + kullanilanDakika);
+        System.out.println("kargoTakipNo: " + kargoTakipNo);
+        System.out.println("kuponSuresi: " + kuponSuresi);
+        System.out.println("kuponKodu: " + kuponKodu);
+
+        System.out.println("faturaTarihi: " + faturaTarihi);
+
+    }
+}
